386fea096fd7eab29427e794beb97c958d2b474b
sergiorbf <sbittencourt19@gmail.com>
2025-01-08 11:53:57 -0300

    creating sidebar and main pages

---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,78 @@
+import { Bed, Home, MapPin, Search, Settings, Sun, Utensils } from "lucide-react";
+import Link from "next/link";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Home",
+    url: "/home",
+    icon: Home,
+  },
+  {
+    title: "Destination",
+    url: "/destination",
+    icon: MapPin,
+  },
+  {
+    title: "Gastronomy",
+    url: "/gastronomy",
+    icon: Utensils,
+  },
+  {
+    title: "Activity",
+    url: "/activity",
+    icon: Sun,
+  },
+  {
+    title: "Hosting",
+    url: "/hosting",
+    icon: Bed,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Menu</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
