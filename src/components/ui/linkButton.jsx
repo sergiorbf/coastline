12d928cc05a1dc49780e3143603e1895a740bd79
sergiorbf <sbittencourt19@gmail.com>
2025-03-20 16:18:17 -0300

    a

--- src/components/ui/linkButton.jsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import { Button } from "./button";
-export const LinkButton = ({ href, buttonText }) => {
-    return (<Link href={href}>
-      <Button variant="ghost" className="text-blue-600 hover:underline">
-        {buttonText}
-      </Button>
-    </Link>);
-};
