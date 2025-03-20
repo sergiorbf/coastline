89e786f5d61701b758105388578e105c28d4c8b1
sergiorbf <sbittencourt19@gmail.com>
2025-03-20 08:55:08 -0300

    remove postgres settings and starting with docker config

--- src/lib/db.js
+++
@@ -1,6 +0,0 @@
-import postgres from 'postgres';
-// Conectar ao banco de dados usando a URL definida no .env
-const sql = postgres(process.env.DATABASE_URL, {
-    ssl: false, // Se o banco estiver na nuvem, pode precisar de { ssl: 'require' }
-});
-export default sql;
